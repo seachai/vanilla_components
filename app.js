c811b5aaa8eafc3a1fc295991ba34b0176461ee1
Chai Lee <imchai@gmail.com>
2019-12-20 16:50:52 -0500

    Added the top scrolling bar

--- app.js
+++ app.js
@@ -56,3 +56,14 @@ const toggleModal = () => {
 };
 
 toggleModal();
+
+const colorBar = document.getElementById("color-bar");
+
+window.addEventListener("scroll", e => {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+});
